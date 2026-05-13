8a13f87fd8e800302c18b035d835bae2f6f1ef0f
XieFangcheng <719766929@qq.com>
2026-05-13 14:03:41 +0800

    init vitepress homework

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,34 @@
+export default {
+  title: "我的作业文档",
+  description: "学习作业提交",
+
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "LeetCode", link: "/leetcode/" },
+      { text: "课堂练习", link: "/practice/" },
+      { text: "大作业", link: "/project/" }
+    ],
+
+    sidebar: {
+      "/leetcode/": [
+        {
+          text: "LeetCode 题解",
+          items: [{ text: "1041 困于环中的机器人", link: "/leetcode/1041" }]
+        }
+      ],
+      "/practice/": [
+        {
+          text: "课堂练习",
+          items: [{ text: "补全两个方法", link: "/practice/homework" }]
+        }
+      ],
+      "/project/": [
+        {
+          text: "大作业",
+          items: [{ text: "选题说明", link: "/project/topic" }]
+        }
+      ]
+    }
+  }
+}
